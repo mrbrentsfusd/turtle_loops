725c762ecf29f6da8e5c78df72baafe07df43b28
hellapythonic <brent@hellapythonic.com>
2024-10-17 16:11:59 -0700

    adeded challenge code

---
+++ challenge.py
@@ -0,0 +1,8 @@
+# Here's your challenge: Use the starter code below: write a for loop that creates a hexagon (6-sided geometric shape). 
+
+# BONUS SHINY CHALLENGE:
+# 1. Use variables in place of the numbers.
+# 2. Write your program so it asks the user how many sides the shape should have. 
+# 3. Write the code inside the for loop to use the number of sides to determine the angle.
+
+# YOUR CODE HERE
